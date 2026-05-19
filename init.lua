ceb96a2bd606a562f9f22b347f7cf447cd0522bc
Marcel Kerkveld <4930603+mke21@users.noreply.github.com>
2026-05-19 08:10:55 +0200

    beter autocomplete

--- init.lua
+++ init.lua
@@ -36,6 +36,22 @@ local cmp = require("cmp")
 
 cmp.setup({
   mapping = {
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback() -- normaal Tab gedrag
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
   },
