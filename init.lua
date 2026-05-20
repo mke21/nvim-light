a8f4fcaa0a708a9b20be1ed0b4c542a45dab77b3
Marcel Kerkveld <4930603+mke21@users.noreply.github.com>
2026-05-20 10:54:46 +0200

    mini center

--- init.lua
+++ init.lua
@@ -12,13 +12,21 @@ vim.pack.add({
 })
 require("oil").setup()
 require("mason").setup()
+  local win_config = function()
+    local height = math.floor(0.618 * vim.o.lines)
+    local width = math.floor(0.618 * vim.o.columns)
+    return {
+      anchor = 'NW', height = height, width = width,
+      row = math.floor(0.5 * (vim.o.lines - height)),
+      col = math.floor(0.5 * (vim.o.columns - width)),
+    }
+  end
+
 require('mini.pick').setup({
   options = {
     use_cache = true,
   },
-  window = {
-      preview = true,
-  },
+  window = { config = win_config },
 })
 
 -- LSP
